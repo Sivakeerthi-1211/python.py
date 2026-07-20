08fe878df58eaa82bad1edb7a2861026dbaa5c2e
sivakeerthi-1211 <eerlasivakeerthi@gmail.com>
2026-07-20 22:08:02 +0530

    IMS Project

---
+++ validation.py
@@ -0,0 +1,29 @@
+# def get_valid_name():
+#     while True:
+#         product_name = input("Enter Product Name: ").strip()
+#         if product_name == "":
+#             print("Product Name cannot be empty.")
+#         else:
+#             return product_name
+
+def get_valid_price():
+    while True:
+        try:
+            product_price = float(input("Enter Product Price: "))
+            if product_price >= 0:
+                return product_price
+            else:
+                print("Price cannot be negative.")
+        except ValueError:
+            print("Invalid Price. Please enter a valid number.")
+
+def get_valid_quantity():
+    while True:
+        try:
+            product_quantity = int(input("Enter Product Quantity: "))
+            if product_quantity >= 0:
+                return product_quantity
+            else:
+                print("Quantity cannot be negative.")
+        except ValueError:
+            print("Invalid Quantity. Please enter a whole number.")
